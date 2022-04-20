129d049271049dc392c175905377865348cf7430
Laura Voss <lauravoss@posteo.de>
2022-04-20 16:58:32 +0100

    created DockingStation class

--- spec/Boris_Bike_spec.rb
+++ spec/Boris_Bike_spec.rb
@@ -2,9 +2,4 @@ require 'boris_bike'
 
 describe DockingStation do
   
-  it 'creates new instance of DockingStation class' do
-    docking_station = DockingStation.new
-
-    expect(docking_station).to eq()
-  end
 end
